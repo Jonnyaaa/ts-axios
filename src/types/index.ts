17b84eca05ef307ffafa52942102dc27c155cc18
Jonnyaaa <3416998350@qq.com>
2025-10-18 11:36:13 +0800

    feat(auth): 实现 HTTP 基本认证（Authorization）功能，支持配置 auth 自动添加请求头

--- src/types/index.ts
+++ src/types/index.ts
@@ -30,6 +30,7 @@ export interface AxiosRequestConfig {
   xsrfHeaderName?: string
   onDownloadProgress?: (e: ProgressEvent) => void
   onUploadProgress?: (e: ProgressEvent) => void
+  auth?: AxiosBasicCredentials
 
   // 允许添加其他任意属性，字符串索引签名
   [propName: string]: any
@@ -136,4 +137,9 @@ export interface Cancel {
 
 export interface CancelStatic {
   new (message?: string): Cancel
+}
+
+export interface AxiosBasicCredentials {
+  username: string
+  password: string
 }
